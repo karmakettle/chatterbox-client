dcfba306ae12a917945f500364b240dee44f864c
MKS-Student <mks.student@hackreactor.com>
2015-03-19 17:21:17 -0500

    completed basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,30 +1,46 @@
 // YOUR CODE HERE:
-//how to do the ajax gets request
-//how does parse store stuff
+//how to break down the ajax gets and post requests
+//how does parse store stuff?
+//Event listeners: best practices?
+
 (function(){
 
 window.app = {
+  storage: null,
   init: function(){
     $('#main').on('click', '.username', function(){
       // console.log("I've been clicked!")
       app.addFriend($(this).text());
-      // console.log(app.friends);
+      console.log(app.friends);
     })
 
-    // $('.submit').click(function() {
-    //   var message = {
-    //     text: $('.text').val(),
-    //     username: window.location.search.slice(10),
-    //     roomname: '' //whut
-    //   };
-    // })
+    $('#main').on('click', '.submit', function() {
+      console.log('click handler activated!')
+      app.submitHandler()
+    })
 
-    // $('option').click(function() {
-    //   $('.active').removeClass('active');
-    //   $(this).addClass('active');
-    // })
+    $('#roomSelect').on('change', function() {
+      var roomnameCache = $('#roomSelect option:selected').text()
+      if ( roomnameCache === "Create New Room..." ) {
+        var newRoom = prompt("Choose a room name: ");
+        app.addRoom(newRoom);
+        roomnameCache = newRoom;
+      } 
 
-    // console.log("Handler count:", jQuery._data( $('#main')[0], "events" ).click.length)
+      //clear the dom
+      var data = app.storage;
+      app.clearMessages();
+      
+      app.roomFilter(data, roomnameCache)
+    })
+  },
+    //filter and add messages from the clicked room
+  roomFilter: function(data, roomnameCache){
+    data.results.filter(function(message){
+      return message.roomname === roomnameCache;
+    }).forEach(function(message){
+      app.addMessage(message);
+    })
   },
   send: function(message){
     //post request via ajax
@@ -35,6 +51,7 @@ window.app = {
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
+        app.fetch(); //should we be fetching?
       },
       error: function (data) {   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
@@ -43,42 +60,53 @@ window.app = {
   },
   addMessage: function(message){
     //adds messages to the DOM
-    $('#chats').prepend('<div><a href="#" class="username">' + message.username + '</a>: ' + message.text + '</div>');
+    $('#chats').append('<div><a href="#" class="username">' + message.username + '</a>: ' + message.text + '</div>');
   },
   clearMessages: function(){
-    //clear messages from the DOM
     $('#chats').empty();
-    // console.log($('#chats').children());
   },
   addRoom: function(roomname){
-    $('#roomSelect').append('<option>' + roomname + '</option>');
+    $('#roomSelect').append('<option class="active">' + roomname + '</option>');
   },
   fetch: function(){
     // $.get(function(data){
     //   console.log(data);
     // }) 
+    app.clearMessages();
 
     $.ajax('https://api.parse.com/1/classes/chatterbox', {
-      // url: this.url,
       type: 'GET',
-      // data: JSON.stringify(message),
+      data: {'order':'-createdAt'},
       contentType: 'application/json',
       success: function (data) {
+        app.roomFilter(data, $('#roomSelect option:selected').text())
+        app.storage = data  
+
         console.log('chatterbox: Messages received');
       },
       error: function (data) {   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to retrieve messages');
       }
     })
-
   },
   friends: {},
   addFriend: function(username) {
-    // alert('hi')
     app.friends[username] = true;
   },
+  submitHandler: function() {
+    var message = {
+      text: $('.text').val(),
+      username: decodeURI(window.location.search.slice(10)),
+      roomname: $('#roomSelect option:selected').text()
+    };
+    app.send(message)
+  },
+
   server: 'https://api.parse.com/1/classes/chatterbox'
 };
+
+app.init()
+app.fetch()
 
 })();
 
